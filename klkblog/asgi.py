ffd9df589105d31b8ce22a4eef4967c2ab8e81ed
lorenzochahid <chahid.lorenzo@protonmail.com>
2020-07-31 10:30:36 +0200

    first commit

---
+++ klkblog/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for klkblog project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'klkblog.settings')
+
+application = get_asgi_application()
